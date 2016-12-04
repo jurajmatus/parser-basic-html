e1a1a24e1022dbdcee47e62ea25d609fdc6bbdf7
Marian Carnoky <majo.carnoky@gmail.com>
2016-12-04 14:38:39 +0100

    [TASK] finnished rule definition

--- src/main/java/parsing/Parser.java
+++ src/main/java/parsing/Parser.java
@@ -66,7 +66,74 @@ public class Parser implements Closeable, AutoCloseable {
 		Rule.define("metatag", t("<meta"), t("name="), n("word"), t("content="), n("word"), t(">"))
 			.register(ruleTable, ruleList, t("<meta"));
 		
-		// TODO - fill in remaining rules here
+		// TODO - fill in remaining rules here ******************************
+		Rule.define("documentbody", t("<body>"), n("bodytags"), t("</body>"))
+				.register(ruleTable, ruleList, t("<body>"));
+		Rule.define("bodytags", n("bodytag"), n("bodytags"))
+				.register(ruleTable, ruleList, t("<table>"),t("<ul>"),t("<ol>"),t("<dl>"),t("<p>"));
+		Rule.define("bodytags")
+				.register(ruleTable, ruleList, t("</body>"),t("</html>"),t("</p>"),t("</td>"));
+		Rule.define("bodytag", n("table"))
+				.register(ruleTable, ruleList, t("<table>"));
+		Rule.define("bodytag", n("list"))
+				.register(ruleTable, ruleList, t("<ul>"),t("<ol>"),t("<dl>"));
+		Rule.define("bodytag", n("paragraph"))
+				.register(ruleTable, ruleList, t("<p>"));
+		Rule.define("bodytag", n("content"))
+				.register(ruleTable, ruleList,TokenStream.LETTER, TokenStream.DIGIT, TokenStream.OTHERSYMBOL,t("<table>"),t("<ul>"),t("<ol>"),t("<dl>"),t("<p>"),t("</body>"),t("</html>"),t("</p>"),t("</td>"));
+		Rule.define("paragraph", t("<p>"),n("bodytags"),n("paragraphend"))
+				.register(ruleTable, ruleList,t("<p>"));
+		Rule.define("paragraphend", t("</p>"))
+				.register(ruleTable, ruleList,t("</p>"));
+		Rule.define("paragraphend")
+				.register(ruleTable, ruleList, t("<table>"),t("<ul>"),t("<ol>"),t("<dl>"),t("<p>"),t("</body>"),t("</html>"),t("</p>"),t("</td>"));
+		Rule.define("table",t("<table>"),n("tablerows"), t("</table>"))
+				.register(ruleTable, ruleList, t("<table>"));
+		Rule.define("tablerows",n("tablerow"),n("tablerows"))
+				.register(ruleTable, ruleList, t("<tr>"));
+		Rule.define("tablerows")
+				.register(ruleTable, ruleList, t("</table>"));
+		Rule.define("tablerow",t("<tr>"),n("tablecells"),t("</tr>"))
+				.register(ruleTable, ruleList, t("<tr>"));
+		Rule.define("tablecells",n("tablecell"),n("tablecells"))
+				.register(ruleTable, ruleList, t("<td>"));
+		Rule.define("tablecells")
+				.register(ruleTable, ruleList, t("</tr>"));
+		Rule.define("tablecell",t("<td>"),n("bodytags"),n("tablecellend"))
+				.register(ruleTable, ruleList, t("<td>"));
+		Rule.define("tablecellend",t("</td>"))
+				.register(ruleTable, ruleList, t("</td>"));
+		Rule.define("tablecellend")
+				.register(ruleTable, ruleList, t("<td>"), t("</tr>"));
+		Rule.define("list",n("unordered"))
+				.register(ruleTable, ruleList, t("<ul>"));
+		Rule.define("list",n("ordered"))
+				.register(ruleTable, ruleList, t("<ol>"));
+		Rule.define("list",n("definitionlist"))
+				.register(ruleTable, ruleList, t("<dl>"));
+		Rule.define("unordered",t("<ul>"),n("listitems"),t("</ul>"))
+				.register(ruleTable, ruleList, t("<ul>"));
+		Rule.define("ordered",t("<ol>"),n("listitems"),t("</ol>"))
+				.register(ruleTable, ruleList, t("<ol>"));
+		Rule.define("listitems",t("<li>"),n("bodytag"),n("listitems"))
+				.register(ruleTable, ruleList, t("<ol>"));
+		Rule.define("listitems")
+				.register(ruleTable, ruleList, t("</ul>"),t("</ol>"));
+		Rule.define("definitionlist",t("<dl>"),n("defterms"),t("</dl>"))
+				.register(ruleTable, ruleList,t("<dl>"));
+		Rule.define("defterms",n("defterm"),n("defterms"))
+				.register(ruleTable, ruleList,t("<dt>"),t("<dd>"));
+		Rule.define("defterms")
+				.register(ruleTable, ruleList,t("</dl>"));
+		Rule.define("defterm",t("<dt>"),n("bodytag"))
+				.register(ruleTable, ruleList,t("<dt>"));
+		Rule.define("defterm",t("<dd>"),n("bodytag"))
+				.register(ruleTable, ruleList,t("<dd>"));
+
+
+
+
+		//************************************
 		
 		Rule.define("content", n("word"))
 			.register(ruleTable, ruleList, TokenStream.LETTER, TokenStream.DIGIT, TokenStream.OTHERSYMBOL);
